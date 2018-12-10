415410898ffac1e6541ef1dc622fb243e21bd396
Andres Vega <andres.vega@symplifica.com>
2018-12-09 23:04:52 -0500

    Se agregan funcionalidades - Material-ui - convert-units - Se vuelve dinamico el campo de ciudad

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,55 +1,81 @@
 import React, { Component } from 'react';
-import Location from './Location';
-import WeatherData from './WeatherData';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { PropTypes } from 'prop-types';
 import './styles.css';
 
-import { SUN, WINDY } from '../../constans/weathers';
+import { SUN } from '../../constans/weathers';
+
+import transformWeather from '../../services/transformWeather';
+import getUrlWeatherByCity from '../../services/getUrlWeatherByCity';
+
+import Location from './Location';
+import WeatherData from './WeatherData';
 
 const data = {
-    temperature: 5,
-    weatherState: SUN,
-    humidity: 10,
-    wind: '10 m/s'
-};
-
-const data2 = {
-    temperature: 15,
-    weatherState: WINDY,
-    humidity: 15,
-    wind: '23 m/s'
+	temperature: 5,
+	weatherState: SUN,
+	humidity: 10,
+	wind: '10 m/s'
 };
 
 class WeatherLocation extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-            city: 'Sao Paulo',
-            data: data
-        };
-    }
+	constructor(props) {
+		super(props);
+		const { city } = props;
 
-    handleUpdateClick = () => {
-        // #debug#
-        console.log('Actualizado');
+		this.state = {
+			city: city,
+			data: null
+		};
+		//#debug#
+		console.log('constructor');
+	}
 
-        this.setState({
-            city: 'Buenos Aires',
-            data: data2
-        });
+	componentDidMount() {
+		//#debug#
+		console.log('componentDidMount');
+		this.handleUpdateClick();
+	}
+	
+	componentDidUpdate(prevProps, prevState) {
+		//#debug#
+		console.log('componentDidUpdate');
+	}
 
-    }
+	handleUpdateClick = () => {
+		const api_weather = getUrlWeatherByCity( this.state.city );
+		fetch( api_weather )
+			.then( ( resolve ) => {
+				return resolve.json();
+			} )
+			.then( data => {
+				const newData = transformWeather(data);
+				this.setState({
+					data: newData
+				});
+			} )
+			.catch( err => console.log("error", err) );
 
-    render() {
-        const { city, data } = this.state;
-        return (
-            <div className="weatherLocationCont">
-                <Location city={ city }></Location>
-                <WeatherData data={ data}></WeatherData>
-                <button onClick={ this.handleUpdateClick }>Actualizar</button>
-            </div>
-        );
-    }
+	}
+
+	render() {
+		//#debug#
+		console.log('render');
+		const { city, data } = this.state;
+		return ( <div className = "weatherLocationCont">
+			<Location city = { city }/>
+			{ data ? 
+				<WeatherData data = { data }/> :
+				<CircularProgress size={50} />
+			}
+			</div>
+		);
+	}
+}
+
+WeatherLocation.propTypes = {
+	city: PropTypes.string.isRequired,
 }
 
 export default WeatherLocation;
---
+++ src/constans/api_url.js
@@ -0,0 +1,3 @@
+
+export const api_key = '110b5115f8d8beef34d13261c3e82c7a';
+export const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
